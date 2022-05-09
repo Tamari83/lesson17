d9155a8b7231e41a082ba1456f82737b6c776bbf
Tamari83 <102482968+Tamari83@users.noreply.github.com>
2022-05-09 19:32:21 +0400

    add post method(it isn't working

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let mainWrapperPost = document.getElementById('post-block');
 let overLayContent = document.getElementById('overLay');
 let closeOverLay = document.getElementById('close');
 let content = document.getElementById('content');
+let addButton = document.getElementById('add');
+let postOverLay = document.getElementById('postOverLay');
+let form = document.getElementById('form');
+let buttonPost = document.getElementById('post-Button');
+let appearPost = document.getElementById('div-appearPost');
 
 // ვაგზავნი მოთხოვნას
 function ajax(url,callback) {
@@ -10,7 +15,8 @@ function ajax(url,callback) {
     request.addEventListener('load',function() {
   let data=JSON.parse(request.responseText);
      callback(data);
-    
+    // ?????    ეს ცოტა ვერ გავიგე , აქ რას აკეთებს
+     
     });
     request.send();
 }
@@ -31,6 +37,7 @@ function createPost(item){
     divWrapper.classList.add('posts');
     // data-id -ს ვანიჭებთ მნიშვნელობას id ,რის შემდეგც მასში ვარდებ პოსტის id -ის ნომერი.
     divWrapper.setAttribute('data-id',item.id);
+    // ????ID-ისთვის მნიშვნელობის მინიჭება შემიძლია თუ არა , როდესაც ვიყენებ createElementByid,an classlist.add
 
     let h2Tag = document.createElement('h2');
     h2Tag.innerText = item.id;
@@ -101,3 +108,63 @@ closeOverLay.addEventListener('click',function(){
     overLayContent.classList.remove('active');
     content.innerHTML = ' ';
 })
+
+addButton.addEventListener('click',function(){
+    postOverLay.classList.add('active-add');
+})
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    console.log(event.target);
+
+
+
+// როგორ დავამატოთ ბ102 პოსტი რაც სერვერის ლინკზე არ არის.
+    let formData = {
+        title:event.target[0].value,
+        title:event.target[1].value
+
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+    //    მომხმარებელი ინფორმაციას რომ ჩაწერრს და დააწვება  პოსტის დამატებას ინფორმაცია უნდა წაიშალოს
+         postOverLay.classList.remove('active-add');
+         console.log(formData);
+})
+buttonPost.addEventListener('click',function(event) {
+
+    appearPost.classList.add('appear-div');
+
+    let divPPost= document.createElement('p');
+    divPPost.innerText = formData.event.target[0];
+
+    let divPPosts= document.createElement('p');
+    divPPost.innerText = event.target[1];
+    
+    div-appearPost.appendChild(divPPost);
+    div-appearPost.appendChild(divPPosts);
+
+    buttonPost.classList.add('post-active');
+    console.log(div-appearPost);
+
+
+})
+
+
+
+
+
+
+
+
+
+
+ 
